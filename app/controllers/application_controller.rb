1ba981ca21a5e778abc370233c8a7cd52215a8fe
Mauricio <mauricio.b@cincoveinticinco.com>
2022-06-23 15:06:36 -0500

    remove authorization application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::API
-<<<<<<< HEAD
-	# before_action :authorize_request
-=======
->>>>>>> d1e176c975d8ede7637f304239141a8bb66f174e
 	
 	def checkLocation(info)
 		el = Location.find_by(location_name: info)
