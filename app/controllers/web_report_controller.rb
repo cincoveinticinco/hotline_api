e419e150f2d4d7823aafcb03b7257569615ebfee
Mauricio <mauricio.b@cincoveinticinco.com>
2022-06-24 13:00:05 -0500

    answer

--- app/controllers/web_report_controller.rb
+++ app/controllers/web_report_controller.rb
@@ -7,7 +7,6 @@ class WebReportController < ApplicationController
 		else
 			rp = Report.find(params['report_id'])
 		end
-
 		answers.each do |a|
 			qt = Question.find(a['question_id'])
 			anw = Answer.find_by(:report_id => rp.id, :question_id => qt.id)
@@ -17,14 +16,12 @@ class WebReportController < ApplicationController
 			anw.a_yes_or_no = a['a_yes_or_no'] if qt.q_type_id == 2 || qt.q_type_id == 3
 			anw.q_option_id	= a['q_option_id'] if qt.q_type_id == 4
 			anw.save
-
 			rp.update(r_email: a['answer']) if qt.id == 5
-			# Creates reference number for followup
 			if qt.id == 18 and rp.r_reference.nil?
 				rp.update(r_reference: SecureRandom.hex(10), r_status_id: 2)
-				answer = Answer.where(report_id: rp.id).where("question_id IN (?)", [11, 12])
-				name = answer.select { |obj| obj.question_id == 11 }.first
-				season = answer.select { |obj| obj.question_id == 12 }.first
+				answers_proj = Answer.where(report_id: rp.id).where("question_id IN (?)", [11, 12])
+				name = answers_proj.select { |obj| obj.question_id == 11 }.first
+				season = answers_proj.select { |obj| obj.question_id == 12 }.first
 				if name && season
 					project = Project.where("p_name = ? AND p_season = ?", name, season).take
 					rp.update(project_id: project.id) if project
