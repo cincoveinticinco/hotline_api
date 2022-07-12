1bc0af78448afbc63d458dc42f3526033f9ea769
Alexander Ospina <alexander.ospina@cincoveinticinco.com>
2022-07-12 17:30:41 -0500

    .

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,11 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  def encode_token_report(report)
+    report.update(token_last_update: DateTime.now)
+      payload = { id: report.id, reference: report.r_reference}
+      JWT.encode(payload, Rails.application.credentials.secret_key_base, 'HS256')
+  end
+
 end
